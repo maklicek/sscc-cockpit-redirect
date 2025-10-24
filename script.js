4fce8bc3f1f4ec8839863d2bb06026901398577a
Martin Klíč <126399810+maklicek@users.noreply.github.com>
2025-10-24 17:55:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function loadModule(lang) {
-  const url = lang === 'cz' ? 'SSCC_ExportCZ/export-cz.html' : 'SSCC_ExportEN/export-en.html';
-  fetch(url)
+ const url = lang === 'cz' ? './SSCC_ExportCZ/export-cz.html' : './SSCC_ExportEN/export-en.html';
     .then(response => {
       if (!response.ok) throw new Error('Soubor nenalezen');
       return response.text();
